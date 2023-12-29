8a08495b21b7a9871f48b5ffa170bb6731df0c29
null0ptr <nazshak6@gmail.com>
2023-12-29 21:29:35 +0600

    changes arise due to login page, add some new rules in eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,8 @@ module.exports = {
     'semi': ['error', 'never'],
     'import/no-absolute-path': 'off',
 		'no-tabs': ['error', { allowIndentationTabs: true }],
-		"react/jsx-indent": [1, 'tab', {checkAttributes: true, indentLogicalExpressions: true}]
+		"react/jsx-indent": [1, 'tab', {checkAttributes: true, indentLogicalExpressions: true}],
+		"react/jsx-indent-props": [1, 'tab'],
+		"max-len": ["warn", { "code": 80, "tabWidth": 2 }]
   },
 }
