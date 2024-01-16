8bac71b8c543013c204d750753fdd9887a66b426
null0ptr <nazshak6@gmail.com>
2024-01-16 22:47:49 +0600

    add validation functionality to all elements except map and image uploader; modified some styles of the elements; used them as utility

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -5,8 +5,14 @@ import {
 	Box,
 	FormControl,
 } from '@mui/material'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import ImageUploader from './Utility/ImageUploader'
 import GetLocation from './Utility/GetLocation'
+
 import {
 	AvailableDate,
 	Contact,
@@ -18,15 +24,59 @@ import {
 	SubmitButton,
 } from './Utility/AddPropertyElements'
 
+const PropertySchema = z.object({
+	title: z
+		.string()
+		.max(5, 'Title length should be less than or equal to 5 characters')
+		.refine((data) => {
+			const actualData = data.trimEnd().trimStart()
+			return actualData !== ''
+		}, {
+			message: 'Title is required',
+		}),
+	date: z
+		.string()
+		.refine((value) => {
+			const dif = Math.ceil((new Date(value) - Date.now()) / (1000 * 60 * 60 * 24))
+			return dif <= 366 && dif >= 0
+		}, 'Select a day between today and the next 365 days'),
+	gender: z.string().min(1, 'Select a gender'),
+	description: z.string()
+		.max(10, 'You can use at most 10 characters')
+		.refine((data) => {
+			const actualData = data.trimEnd().trimStart()
+			return actualData !== ''
+		}, {
+			message: 'Rules and Preferences are required',
+		}),
+	rules_and_preference: z
+		.string()
+		.max(10, 'You can use at most 10 characters')
+		.optional(),
+	price: z
+		.string()
+		.refine((data) => parseInt(data, 10) >= 0, {
+			message: 'Price should be postive',
+		}),
+	contact: z.string()
+		.refine((data) => data !== '', {
+			message: 'Contact is required',
+		}),
+})
 export default function AddProperty() {
 	const [location, setLocation] = useState(null)
-
-	const handleSubmit = (event) => {
-		event.preventDefault()
-		const data = new FormData(event.currentTarget)
-		data.append('location', JSON.stringify(location))
-		console.log(data)
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+		reset,
+		getValues,
+	} = useForm({ resolver: zodResolver(PropertySchema), defaultValues: { gender: '' } })
+	const onSubmit = (event) => {
+		console.log('here')
+		console.log(event)
 	}
+
 	return (
 		<Box
 			component="div"
@@ -36,17 +86,17 @@ export default function AddProperty() {
 			}}
 		>
 			<Box component="div">
-				<FormControl fullWidth component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-					<Header />
-					<AvailableDate />
-					<GenderSelection />
-					<PlaceDescription />
-					<RulesAndPreference />
-					<Price />
-					<Contact />
+				<FormControl fullWidth component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+					<Header register={register('title')} error={errors.title} />
+					<AvailableDate register={register('date')} error={errors.date} />
+					<GenderSelection register={register('gender')} error={errors.gender} />
+					<PlaceDescription register={register('description')} error={errors.description} />
+					<RulesAndPreference register={register('rules_and_preference')} error={errors.rules_and_preference} />
+					<Price register={register('price')} error={errors.price} />
+					<Contact register={register('contact')} error={errors.contact} />
 					<ImageUploader />
 					<GetLocation getLocation={setLocation} />
-					<SubmitButton />
+					<SubmitButton isSubmitting={isSubmitting} />
 				</FormControl>
 			</Box>
 		</Box>
--- src/components/Utility/AddPropertyElements.jsx
+++ src/components/Utility/AddPropertyElements.jsx
@@ -4,125 +4,222 @@ import {
 } from 'react'
 import {
 	Button,
+	FormControl,
+	FormHelperText,
 	MenuItem,
 	Select,
 	TextField,
 	TextareaAutosize,
 } from '@mui/material'
+import '../../styles/forms.css'
 
-const Header = memo(() => (
-	<TextField
-		margin="normal"
-		required
-		fullWidth
-		placeholder="Give a title"
-		name="title"
-		label="Title"
-		type="text"
-		id="title"
-	/>
-))
-const AvailableDate = memo(() => (
-	<TextField
-		margin="dense"
-		required
-		fullWidth
-		name="date"
-		type="date"
-		id="date"
-		label="Available Date"
-		InputLabelProps={{
-			shrink: true,
-		}}
-	/>
-))
-const Price = memo(() => (
-	<TextField
-		margin="normal"
-		required
-		fullWidth
-		name="price"
-		type="number"
-		id="price"
-		label="Price"
-	/>
-))
-const Contact = memo(() => (
-	<TextField
-		margin="normal"
-		required
-		fullWidth
-		name="contact"
-		type="tel"
-		id="contact"
-		label="Contact"
-	/>
-))
-const GenderSelection = memo(() => {
+const Header = memo(({ register, error }) => {
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	console.log('header')
+	return (
+		<TextField
+			margin="normal"
+			name={name}
+			fullWidth
+			placeholder="Give a title"
+			label="Title"
+			type="text"
+			id="title"
+			onChange={onChange}
+			onBlur={onBlur}
+			ref={ref}
+			error={Boolean(error)}
+			helperText={error ? error.message : ''}
+		/>
+	)
+})
+const AvailableDate = memo(({ register, error }) => {
+	console.log('date')
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	return (
+		<TextField
+			margin="normal"
+			fullWidth
+			name={name}
+			type="date"
+			id="date"
+			label="Available Date"
+			InputLabelProps={{
+				shrink: true,
+			}}
+			onChange={onChange}
+			onBlur={onBlur}
+			ref={ref}
+			error={Boolean(error)}
+			helperText={error ? error.message : ''}
+		/>
+	)
+})
+const Price = memo(({ register, error }) => {
+	console.log('price')
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	return (
+		<TextField
+			margin="normal"
+			name={name}
+			fullWidth
+			type="number"
+			id="price"
+			label="Price"
+			onChange={onChange}
+			onBlur={onBlur}
+			ref={ref}
+			error={Boolean(error)}
+			helperText={error ? error.message : ''}
+		/>
+	)
+})
+const Contact = memo(({ register, error }) => {
+	console.log('contact')
+
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	return (
+		<TextField
+			margin="normal"
+			name={name}
+			fullWidth
+			type="tel"
+			id="contact"
+			label="Contact"
+			onChange={onChange}
+			onBlur={onBlur}
+			ref={ref}
+			error={Boolean(error)}
+			helperText={error ? error.message : ''}
+		/>
+	)
+})
+const GenderSelection = memo(({ register, error }) => {
+	console.log('gender')
 	const [gender, setGender] = useState('')
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
 	const handleGender = (event) => {
+		onChange(event)
 		setGender(event.target.value)
 	}
 	return (
-		<Select
-			margin="dense"
-			id="gender"
-			name="gender"
-			displayEmpty
-			renderValue={(value) => (value !== '' ? value : 'Choose One...')}
-			value={gender}
-			onChange={handleGender}
-			required
-			placeholder="Choose One..."
-			fullWidth
-			sx={{
-				color: `${gender !== '' ? 'black' : 'gray'}`,
-			}}
+		<FormControl
+			error={error}
+			margin="normal"
 		>
-			<MenuItem value="male">Male</MenuItem>
-			<MenuItem value="female">Female</MenuItem>
-		</Select>
+			<Select
+				id="gender"
+				displayEmpty
+				name={name}
+				ref={ref}
+				renderValue={(value) => (value !== '' ? value : 'Choose One...')}
+				value={gender}
+				onChange={handleGender}
+				onBlur={onBlur}
+				placeholder="Choose One..."
+				fullWidth
+				sx={{
+					color: `${gender !== '' ? 'black' : 'gray'}`,
+				}}
+			>
+				<MenuItem value="male">Male</MenuItem>
+				<MenuItem value="female">Female</MenuItem>
+			</Select>
+			<FormHelperText>{error ? error.message : ''}</FormHelperText>
+		</FormControl>
 	)
 })
-const PlaceDescription = memo(() => (
-	<TextareaAutosize
-		id="description"
-		name="description"
-		label="Description"
-		aria-label="description"
-		minRows={5}
-		maxRows={7}
-		placeholder="What this place offers..."
-		style={{
-			width: '100%',
-			padding: '8px',
-			boxSizing: 'border-box',
-			fontSize: '1em',
-			resize: 'none',
-		}}
-	/>
-))
-const RulesAndPreference = memo(() => (
-	<TextareaAutosize
-		id="rules_and_preference"
-		name="rules_and_preference"
-		aria-label="rules_and_preference"
-		minRows={5}
-		maxRows={7}
-		placeholder="Rules and Preference..."
-		style={{
-			width: '100%',
-			padding: '8px',
-			boxSizing: 'border-box',
-			fontSize: '1em',
-			resize: 'none',
-		}}
-	/>
-))
-const SubmitButton = memo(() => (
+const PlaceDescription = memo(({ register, error }) => {
+	console.log('description')
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	return (
+		<FormControl
+			error={error}
+			margin="normal"
+			aria-hidden="true"
+		>
+			<TextareaAutosize
+				id="description"
+				name={name}
+				label="Description"
+				aria-label="description"
+				minRows={5}
+				maxRows={7}
+				placeholder="What this place offers..."
+				className={`place-text-area ${error ? 'place-text-area-error' : 'place-text-area-normal'}`}
+				onChange={onChange}
+				onBlur={onBlur}
+				ref={ref}
+			/>
+			<FormHelperText>{error ? error.message : ''}</FormHelperText>
+		</FormControl>
+	)
+})
+const RulesAndPreference = memo(({ register, error }) => {
+	console.log('rules_and_preference')
+	const {
+		onChange,
+		onBlur,
+		name,
+		ref,
+	} = register
+	return (
+		<FormControl
+			error={error}
+			margin="normal"
+		>
+			<TextareaAutosize
+				id="rules_and_preference"
+				name={name}
+				aria-label="rules_and_preference"
+				minRows={5}
+				maxRows={7}
+				aria-hidden="true"
+				className={`place-text-area ${error ? 'place-text-area-error' : 'place-text-area-normal'}`}
+				placeholder="Rules and Preference..."
+				onChange={onChange}
+				onBlur={onBlur}
+				ref={ref}
+			/>
+			<FormHelperText>{error ? error.message : ''}</FormHelperText>
+		</FormControl>
+	)
+})
+const SubmitButton = memo(({ isSubmitting }) => (
 	<Button
 		type="submit"
 		fullWidth
+		disabled={isSubmitting}
 		variant="outlined"
 		sx={{
 			mt: 3,
