e20b20dec1d924bb76453c5623c2c9ed9d413d59
null0ptr <nazshak6@gmail.com>
2024-02-16 12:28:00 +0600

    add catergory selection type
    fix the label of gender selection

--- src/components/Utility/BasicInfo.jsx
+++ src/components/Utility/BasicInfo.jsx
@@ -11,16 +11,17 @@ const BasicInfo = memo(() => (
 		marginTop="20px"
 		marginBottom="10px"
 		sx={{
-			'@media (min-width: 900px)': {
+			'@media (min-width: 600px)': {
 				display: 'flex',
 				justifyContent: 'space-around',
 			},
 		}}
 	>
 		<Box
+			component="div"
 			marginBottom="10px"
 			sx={{
-				'@media (min-width: 900px)': {
+				'@media (min-width: 600px)': {
 					marginBottom: '0px',
 				},
 			}}
@@ -28,8 +29,28 @@ const BasicInfo = memo(() => (
 			<Typography variant="span" component="section" color="gray" textAlign="center">Available From</Typography>
 			<Typography variant="span" component="section" textAlign="center">12 December, 2020</Typography>
 		</Box>
+		<Box
+			component="div"
+			sx={{
+				marginBottom: '10px',
+				'@media (min-width: 600px)': {
+					marginBottom: '0px',
+				},
+			}}
+		>
+			<Typography
+				variant="span"
+				component="section"
+				color="gray"
+				textAlign="center"
+
+			>
+				Category
+			</Typography>
+			<Typography variant="span" component="section" textAlign="center">Sublet</Typography>
+		</Box>
 		<Box>
-			<Typography variant="span" component="section" color="gray" textAlign="center">Category</Typography>
+			<Typography variant="span" component="section" color="gray" textAlign="center">Gender</Typography>
 			<Typography variant="span" component="section" textAlign="center">Male</Typography>
 		</Box>
 	</Box>
