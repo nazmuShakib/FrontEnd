7c0eb6a5b73a244b62f2f7eb9f0fa1665f568e70
null0ptr <nazshak6@gmail.com>
2024-02-24 23:47:01 +0600

    add lazy loading feature to home page cards
    also, add skeleton loading to taka.png

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,14 @@
 import { memo } from 'react'
-import { Link } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import axios from 'axios'
 import {
 	Box,
-	Card,
-	CardActionArea,
-	CardContent,
-	CardMedia,
 	Grid,
-	Typography,
 	CircularProgress,
 } from '@mui/material'
+import LazyLoad from 'react-lazy-load'
+import AdvertisementCard from '../components/Utility/HomePageCard'
 import '../styles/card.css'
-import Taka from '../assets/icons/taka.svg'
 
 const Home = memo(() => {
 	console.log('home')
@@ -26,44 +21,25 @@ const Home = memo(() => {
 	const allProperties = data?.data.data
 	return (
 		<Box component="div" className="advertisement">
-			<Grid container spacing={2} direction="row" justifyContent="normal" alignItems="center" className="grid">
-				{allProperties.map((property) => {
-					const date = new Date(property.availableDate)
-					const formattedDate = new Intl.DateTimeFormat('en-US', {
-						day: 'numeric',
-						month: 'long',
-						year: 'numeric',
-					}).format(date)
-					return (
-						<Grid key={Number(Math.random())} item className="advertise-card">
-							<Card className="card">
-								<CardActionArea component={Link} to="/property" state={{ from: property }}>
-									<CardMedia component="img" image={property.thumbnail} loading="lazy" height="200px" />
-									<CardContent className="card-content">
-										<Box component="div" display="flex" alignItems="center" justifyContent="normal" gap="10px">
-											<img alt="taka" src={Taka} loading="lazy" id="advertise-taka" />
-											<Typography variant="h6" component="h1">
-												{property.price}
-											</Typography>
-										</Box>
-										<Typography
-											variant="subtitle2"
-											component="span"
-										>
-											{formattedDate}
-										</Typography>
-										<Typography variant="body2" component="div">
-											{`${property.placeInfo.thana}, ${property.placeInfo.district}`}
-										</Typography>
-										<Typography variant="body1" component="article" sx={{ overflowWrap: 'break-word', hyphens: 'auto' }}>
-											{property.title}
-										</Typography>
-									</CardContent>
-								</CardActionArea>
-							</Card>
-						</Grid>
-					)
-				})}
+			<Grid
+				container
+				spacing={2}
+				direction="row"
+				justifyContent="normal"
+				alignItems="center"
+				className="grid"
+			>
+				{allProperties.map((property) => (
+					<Grid key={property.ID} item className="advertise-card">
+						<LazyLoad
+							height={400}
+							offsetVertical={200}
+							threshold={0.20}
+						>
+							<AdvertisementCard key={property.ID} property={property} />
+						</LazyLoad>
+					</Grid>
+				))}
 			</Grid>
 		</Box>
 	)
