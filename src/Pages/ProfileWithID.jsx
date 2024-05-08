5f9a8d29a3219520575a1fd9dc65d15d8e1d5cd1
nazmuShakib <nazmushakib6@gmail.com>
2024-05-08 23:56:54 +0600

    implement public user profile page
    now, users can view other users username, email and the properties the user has uploaded

--- src/Pages/ProfileWithID.jsx
+++ src/Pages/ProfileWithID.jsx
@@ -4,9 +4,13 @@ import { useQuery } from 'react-query'
 import {
 	Box, Typography,
 	Avatar, CircularProgress,
+	Grid,
 } from '@mui/material'
 import { AttachFileOutlined, SendOutlined } from '@mui/icons-material'
+import LazyLoad from 'react-lazy-load'
+import AdvertisementCard from '../components/Utility/HomePageCard'
 import '../styles/profile.css'
+import '../styles/card.css'
 import axios from '../api/axios'
 
 function stringToColor(string) {
@@ -60,24 +64,25 @@ const ProfileWithID = memo(() => {
 		return <AttachFileOutlined />
 	}
 	return (
-		<Box
-			component="div"
-			sx={{
-				height: '100%',
-				width: '100%',
-				marginTop: '20px',
-				display: 'flex',
-				justifyContent: 'center',
-			}}
-		>
+		<>
 			<Box
 				component="div"
-				className="profile-container"
+				sx={{
+					height: '100%',
+					width: '100%',
+					marginTop: '20px',
+					display: 'flex',
+					justifyContent: 'center',
+				}}
 			>
-				<Box component="div" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-					{/* eslint-disable-next-line react/jsx-props-no-spreading */}
-					<Avatar {...stringAvatar(userInfo?.username || '')} src="" />
-					{/* <Button
+				<Box
+					component="div"
+					className="profile-container"
+				>
+					<Box component="div" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+						{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+						<Avatar {...stringAvatar(userInfo?.username || '')} src="" />
+						{/* <Button
 						variant="contained"
 						component="label"
 						className="upload-button upload"
@@ -87,16 +92,23 @@ const ProfileWithID = memo(() => {
 						UPLOAD
 						<input accept="image/*" hidden type="file" />
 					</Button> */}
-				</Box>
-				<Box component="div" sx={{ width: '100%' }}>
-					<UserName userID={userID} userInfo={userInfo} />
-					<Box component="div" className="profile-info">
-						<Typography variant="body1" component="span" sx={{ fontWeight: '500', marginRight: '10px' }}>Email:</Typography>
-						<Typography variant="body1" component="span">{userInfo?.email}</Typography>
+					</Box>
+					<Box component="div" sx={{ width: '100%' }}>
+						<UserName userID={userID} userInfo={userInfo} />
+						<Box component="div" className="profile-info">
+							<Typography variant="body1" component="span" sx={{ fontWeight: '500', marginRight: '10px' }}>Email:</Typography>
+							<Typography variant="body1" component="span">{userInfo?.email}</Typography>
+						</Box>
 					</Box>
 				</Box>
 			</Box>
-		</Box>
+			<Box component="div" sx={{ marginTop: '2em' }}>
+				<Box component="div" sx={{ marginLeft: '10px' }}>
+					<Typography variant="body1" component="span" sx={{ fontSize: '24px', fontWeight: '500' }}>User Properties</Typography>
+				</Box>
+				<ShowUserProperties userID={userID} />
+			</Box>
+		</>
 	)
 })
 const UserName = memo(({ userInfo }) => {
@@ -107,6 +119,48 @@ const UserName = memo(({ userInfo }) => {
 				<Typography variant="body1" component="span" sx={{ fontWeight: '500', marginRight: '10px' }}>UserName:</Typography>
 				<Typography variant="body1" component="span">{userInfo?.username}</Typography>
 			</Box>
+		</Box>
+	)
+})
+const ShowUserProperties = memo(({ userID }) => {
+	console.log('show user properties')
+	const fetchData = () => axios({
+		url: `/public/get/properties/${userID}`,
+		method: 'GET',
+	})
+	const {
+		isLoading, data, isError, error, refetch,
+	} = useQuery(['user-properties', 'public', userID], fetchData, { retry: 1 })
+	if (isError) return <h1>{error.response.data.message}</h1>
+	if (isLoading) return <CircularProgress />
+	const allProperties = data?.data?.data
+	console.log(allProperties)
+	return (
+		<Box component="div" className="advertisement">
+			<Grid
+				container
+				spacing={2}
+				direction="row"
+				justifyContent="normal"
+				alignItems="center"
+				className="grid"
+			>
+				{allProperties.map((property) => (
+					<Grid key={property.property.ID} item className="advertise-card">
+						<LazyLoad
+							height={400}
+							offsetVertical={200}
+							threshold={0.20}
+						>
+							<AdvertisementCard
+								key={property.property.ID}
+								property={property.property}
+								refetch={refetch}
+							/>
+						</LazyLoad>
+					</Grid>
+				))}
+			</Grid>
 		</Box>
 	)
 })
