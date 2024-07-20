73f899c0b7b13c67b0b9db4cb78f46cb48a7e9d2
nazmuShakib <nazmushakib6@gmail.com>
2024-07-20 13:12:53 +0600

    implement update user name feature with form
    now, username is updated with form and validation criteria

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -6,12 +6,24 @@ import {
 	Avatar, CircularProgress,
 	TextField,
 	Button,
+	FormControl,
 } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { AttachFileOutlined, SendOutlined } from '@mui/icons-material'
 import useAxiosPrivate from '../Hooks/useAxiosPrivate'
 import useNotification from '../Hooks/useNotification'
 import '../styles/profile.css'
 import useAuth from '../Hooks/useAuth'
+
+const userNameSchema = z.object({
+	username: z
+		.string()
+		.max(100, 'max 100 characters')
+		.refine((name) => name.trimStart().trimEnd() !== '', { message: 'required' }),
+})
 
 function stringToColor(string) {
 	let hash = 0
@@ -157,53 +169,116 @@ const Notification = memo(({ time, propertyID, notification }) => {
 const UserName = memo(({ userInfo }) => {
 	console.log('username')
 	const [editOrSave, setEditOrSave] = useState(true)
+
+	const handleClick = async () => {
+		setEditOrSave(false)
+	}
+
+	return (
+		<Box component="div" className="user-name">
+			{
+				!editOrSave ? <UpdateUserName setEditOrSave={setEditOrSave} userInfo={userInfo} /> : (
+					<Box component="div" sx={{ width: '100%', display: 'flex', justifyContent: 'space-around' }}>
+						<Box component="div" className="profile-info" sx={{ width: '90%', marginRight: '10px' }}>
+							<Typography variant="body1" component="span" sx={{ fontWeight: '500', marginRight: '10px' }}>UserName:</Typography>
+							<Typography variant="body1" component="span">{userInfo?.username}</Typography>
+						</Box>
+						<Button variant="contained" onClick={handleClick} className="edit-save-button">Edit</Button>
+					</Box>
+				)
+			}
+		</Box>
+	)
+})
+
+const UpdateUserName = memo(({ setEditOrSave, userInfo }) => {
+	console.log('update user name')
+	const { control, handleSubmit, formState: { isSubmitting, errors } } = useForm({
+		resolver: zodResolver(userNameSchema),
+		defaultValues: {
+			username: userInfo?.username,
+		},
+	})
 	const queryClient = useQueryClient()
 	const { auth } = useAuth()
-	const [userName, setUserName] = useState(userInfo?.username)
 	const axiosPrivate = useAxiosPrivate()
 	const { openNotification } = useNotification()
+
 	const submitUserName = (data) => axiosPrivate({
 		url: '/profile/edit/name',
 		method: 'PATCH',
 		data,
 	})
 	const { mutateAsync } = useMutation(['edit-user-name', auth?.userID], submitUserName)
-	const handleClick = async () => {
+	const onSubmit = async (event) => {
 		try {
-			if (!editOrSave) {
-				const data = {
-					name: userName,
-				}
-				await mutateAsync(data)
-				openNotification('Successfully updated username', 'success')
-				queryClient.setQueryData(['get-user-info', auth?.userID], (prevData) => {
-					const newData = prevData
-					if (newData?.data?.data) {
-						newData.data.data.username = userName
-					}
-					return newData
-				})
+			const { username } = event
+			const data = {
+				name: username,
 			}
-			setEditOrSave((prevState) => !prevState)
+			await mutateAsync(data)
+			openNotification('Successfully updated username', 'success')
+			queryClient.setQueryData(['get-user-info', auth?.userID], (prevData) => {
+				const newData = prevData
+				if (newData?.data?.data) {
+					newData.data.data.username = username
+				}
+				return newData
+			})
 		} catch (err) {
 			openNotification('Failed to update username', 'error')
 		}
-	}
-	const handleChange = (event) => {
-		setUserName(event.target.value)
+		setEditOrSave(true)
 	}
 	return (
-		<Box component="div" className="user-name">
-			{
-				!editOrSave ? <TextField disabled={editOrSave} label="User Name" value={userName} onChange={handleChange} sx={{ width: '90%', marginRight: '10px' }} /> : (
-					<Box component="div" className="profile-info" sx={{ width: '90%', marginRight: '10px' }}>
-						<Typography variant="body1" component="span" sx={{ fontWeight: '500', marginRight: '10px' }}>UserName:</Typography>
-						<Typography variant="body1" component="span">{userName}</Typography>
-					</Box>
-				)
-			}
-			<Button variant="contained" onClick={handleClick} className="edit-save-button">{editOrSave ? 'Edit' : 'Save'}</Button>
-		</Box>
+		<FormControl
+			component="form"
+			onSubmit={handleSubmit(onSubmit)}
+			sx={{
+				width: '100%',
+			}}
+		>
+			<Box
+				component="div"
+				className="user-name"
+				sx={{
+					display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '15px',
+				}}
+			>
+				<UserNameField control={control} error={errors.username} />
+				<Button type="submit" disabled={isSubmitting} variant="contained" className="edit-save-button">Save</Button>
+			</Box>
+		</FormControl>
+	)
+})
+const UserNameField = memo(({ control, error }) => {
+	console.log('update user name')
+	return (
+		<Controller
+			name="username"
+			control={control}
+			render={({
+				field: {
+					onChange,
+					onBlur,
+					ref,
+					name,
+					value,
+				},
+			}) => (
+				<TextField
+					name={name}
+					onBlur={onBlur}
+					ref={ref}
+					label="User Name"
+					value={value}
+					onChange={onChange}
+					sx={{ width: '90%', marginRight: '10px' }}
+					error={!!error}
+					helperText={error ? error.message : 'max 100 characters'}
+				/>
+			)}
+		/>
 	)
 })
 export default Profile
